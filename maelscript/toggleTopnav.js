4a76e651d9797bf18ad03da91f7ff3fb868386b4
theMaelstro <109001176+theMaelstro@users.noreply.github.com>
2023-04-05 15:45:07 +0200

    Responsive Init
    Testing responsive layout ideas.

---
+++ maelscript/toggleTopnav.js
@@ -0,0 +1,17 @@
+function toggleTopnav() {
+  var scrollable = document.getElementById("scrollable");
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+	scrollable.className = "scrollable";
+  } else {
+    x.className = "topnav";
+	scrollable.className = "";
+  }
+  
+  var dropdown = document.getElementById("download-container");
+  if (dropdown.style.display === "block"){
+	dropdown.style.display = "none";
+  }
+  
+}
