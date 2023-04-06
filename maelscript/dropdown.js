2c615d250121f9c3f35e672d3fdfe86bc20f0c28
theMaelstro <maelthedev@gmail.com>
2023-04-06 03:02:31 +0200

    Fixes to dropdown script. No more "sticking" containers.

--- maelscript/dropdown.js
+++ maelscript/dropdown.js
@@ -1,5 +1,27 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
+var links = document.getElementsByTagName("a")
+var containers = document.getElementsByClassName("dropdown-container");
+var scrollable = document.getElementById("scrollable");
+var nav = document.getElementById("myTopnav");
 var i;
+
+
+function hideall() {
+		for (i = 0; i < containers.length; i++) {
+			if (containers[i].style.display === "block") {
+			  containers[i].style.display = "none";
+			}
+		}
+		nav.className = "topnav";
+		scrollable.className = "";
+	}
+function hide(ele) {
+		for (i = 0; i < containers.length; i++) {
+			if (containers[i] != ele && containers[i].style.display === "block") {
+			  containers[i].style.display = "none";
+			}
+		}
+	}
 
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
@@ -7,10 +29,18 @@ for (i = 0; i < dropdown.length; i++) {
 	this.firstElementChild.classList.toggle("fa-caret-up");
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
+	hide(dropdownContent)
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
     } else {
       dropdownContent.style.display = "block";
     }
   });
+}
+
+
+
+
+for (i = 0; i < links.length; i++) {
+	links[i].addEventListener("click", hideall);
 }
